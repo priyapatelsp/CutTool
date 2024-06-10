4ac62a6beb1f284f3a174e1c9f5f943bd29d4d4c
Priya Patel <priyapatelsp3012@gmail.com>
2024-06-09 20:11:26 -0400

    Added required changes

--- challenge-cut/CutTool.py
+++ challenge-cut/CutTool.py
@@ -1,25 +1,23 @@
 import argparse
 
-def cut(file_path, delimiter, fields):
-    with open(file_path, 'r') as file:
+def cut(filePath, delimiter, fields):
+    with open(filePath, 'r') as file:
         for line in file:
             parts = line.strip().split(delimiter)
             output_parts = [parts[field - 1] for field in fields]
             print(delimiter.join(output_parts))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Python implementation of the cut command")
+    parser = argparse.ArgumentParser(description="Python implementation of cut command")
     parser.add_argument("file", help="Path to the file to process")
     parser.add_argument("-d", "--delimiter", default="\t", help="Field delimiter (default is tab)")
     parser.add_argument("-f", "--fields", required=True, type=lambda s: [int(field) for field in s.split(",")],
-    help="Fields to cut, separated by comma (e.g., -f 1,3,5)")    
-    args = parser.parse_args()
+    help="Fields to cut, separated by comma (e.g., -f 1,2,3,5)")    
+    arguments = parser.parse_arguments()
    
-    # fields = [int(field) for field in args.fields.replace(',', ' ').split()]
 
-
-    if args.fields is None:
+    if arguments.fields is None:
         print("Error: You must specify at least one field with -f option")
     else:
-        print(args)
-        cut(args.file, args.delimiter, args.fields)
+        print(arguments)
+        cut(arguments.file, arguments.delimiter, arguments.fields)
